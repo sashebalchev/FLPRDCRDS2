5841089974238a39211ffa0870710184a6926775
sashebalchev <sashebalchev@gmail.com>
2018-08-01 18:55:32 +0100

    Added font style functionality.

--- app/src/main/java/com/dmu/sash/flprdcrds/WordAdapter.java
+++ app/src/main/java/com/dmu/sash/flprdcrds/WordAdapter.java
@@ -3,14 +3,18 @@ package com.dmu.sash.flprdcrds;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+
+import java.lang.reflect.Type;
 
 import io.realm.OrderedRealmCollection;
 import io.realm.RealmBaseAdapter;
@@ -19,6 +23,7 @@ public class WordAdapter extends RealmBaseAdapter<Word> implements ListAdapter{
 
     private int fontColor;
     private int backgroundColor;
+    private Typeface typeface;
 
     private static class ViewHolder{
 
@@ -29,10 +34,17 @@ public class WordAdapter extends RealmBaseAdapter<Word> implements ListAdapter{
     WordAdapter(Fragment fragment, OrderedRealmCollection<Word> data, Activity activity){
         super(data);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
-        backgroundColor = Color.parseColor(sharedPreferences.getString("PREF_COLOR_BGs", "#FFFFFF"));
+        String font = sharedPreferences.getString("PREF_STYLE_FONT", "1");
+        if (font.equals("1")){
+            typeface = Typeface.DEFAULT;
+        } else if (font.equals("2")){
+            typeface = ResourcesCompat.getFont(activity, R.font.hanalei_font_family);
+        }
+        backgroundColor = Color.parseColor(sharedPreferences.getString("PREF_COLOR_BG", "#FFFFFF"));
 //        String fontColor = sharedPreferences.getString("PREF_COLOR_FONT", "Black");
 //        int test = Color.parseColor(fontColor);
 //        System.out.println(test);
+//        typeface =
         fontColor = Color.parseColor(sharedPreferences.getString("PREF_COLOR_FONT", "#000000"));
     }
     @Override
@@ -46,6 +58,8 @@ public class WordAdapter extends RealmBaseAdapter<Word> implements ListAdapter{
             viewHolder.wordWithDef = convertView.findViewById(R.id.fetched_text);
             viewHolder.wordWithDef.setTextColor(fontColor);
             viewHolder.wordWithDef.setBackgroundColor(backgroundColor);
+            viewHolder.wordWithDef.setTypeface(typeface);
+
 //            viewHolder.pronunciation = convertView.findViewById(R.id.pronunciation);
 //            viewHolder.pronunciation.setOnClickListener(listener);
             convertView.setTag(viewHolder);
